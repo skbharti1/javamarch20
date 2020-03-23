ddb156e27017948d0567ae7ac31ff075905d2a01
suhvishal <23210008@mkc.org>
2020-03-23 11:28:08 +0530

    Day1

---
+++ day1/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld{
+    //properties
+    //constructors
+    //inner class
+    //methods
+
+    public static void main(String args[]){
+        System.out.println("Hellworld!.. Welcome to Java");
+        System.out.println("Total Arguemnts are : " + args.length);
+    }
+}
+
+ 
